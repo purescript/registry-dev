e6a3f1ce90e7adeb6131ab035b66d0ab95f6a214
Fabrizio Ferrai <fabrizio.ferrai@gmail.com>
2021-03-24 03:12:59 +0200

    Update CI to use pacchettibotti token

--- default.nix
+++ default.nix
@@ -1,16 +1,6 @@
 { pkgs ? import <nixpkgs> {} }:
 
 let
-  easy-ps = import (
-    pkgs.fetchFromGitHub {
-      owner = "justinwoo";
-      repo = "easy-purescript-nix";
-      rev = "3b4039475c245243716b1e922455a9062c0531da";
-      sha256 = "0fk2r02z86rirg5kggd0vvcgp8h07w7fhp03xng7wjrifljxwann";
-    }
-  ) {
-    inherit pkgs;
-  };
   easy-dhall = import (
     pkgs.fetchFromGitHub {
       owner = "justinwoo";
@@ -25,9 +15,6 @@ let
 in
 pkgs.runCommand "dummy" {
   buildInputs =
-    builtins.attrValues {
-      inherit (easy-ps) purescript spago;
-    } ++
     builtins.attrValues {
       inherit (easy-dhall) dhall-simple dhall-json-simple;
     } ++
