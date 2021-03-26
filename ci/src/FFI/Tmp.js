d1ec723ba3ebd629834dfcf225634875079fc303
Fabrizio Ferrai <fabrizio.ferrai@gmail.com>
2021-03-26 12:48:30 +0200

    Detail TODO

--- ci/src/FFI/Tmp.js
+++ ci/src/FFI/Tmp.js
@@ -10,4 +10,8 @@ exports.mkTmpDirImpl = function() {
 };
 
 
-// TODO: tmpobj.removeCallback();
+// TODO: we should do proper bracketing when creating the temporary directory
+// so that if the program crashes we still remove it.
+// In practice we don't really care since this thing is going to run in CI,
+// so this is more of a nice to have for when we run it locally.
+// To remove the temp directory we can call tmpobj.removeCallback();
