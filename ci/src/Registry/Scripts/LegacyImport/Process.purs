f3f15667248dfaf3bf33d8ec1930db0e7ca9d647
Mike Solomon <mike@meeshkan.com>
2021-10-06 19:37:22 +0300

    Update ci/src/Registry/Scripts/LegacyImport/Process.purs
    Co-authored-by: Thomas Honeyman <admin@thomashoneyman.com>

--- ci/src/Registry/Scripts/LegacyImport/Process.purs
+++ ci/src/Registry/Scripts/LegacyImport/Process.purs
@@ -32,7 +32,7 @@ type ProcessedPackages k a =
 type ProcessedPackageVersions k1 k2 a = ProcessedPackages k1 (Map k2 a)
 
 type NameAndAddress = { address :: Address, name :: RawPackageName }
-type NameOriginalAndAddress = { address :: { owner :: String, repo :: String }, name :: PackageName, original :: RawPackageName }
+type NameOriginalAndAddress = { address :: Address, name :: PackageName, original :: RawPackageName }
 type SemVerAndOriginal = { semVer :: SemVer, original :: RawVersion }
 
 -- | Execute the provided transform on every package in the input packages map
