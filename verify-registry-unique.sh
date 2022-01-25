1149f1b536f55e7005e1111a2025013bf6a6ac3e
Thomas Honeyman <admin@thomashoneyman.com>
2022-01-25 11:27:06 -0800

    Remove task and bcrypt from bower-packages.json

--- verify-registry-unique.sh
+++ verify-registry-unique.sh
@@ -5,9 +5,8 @@ set -euxo pipefail
 
 # This script checks that there are no duplicate entries in the two json files listing packages
 
-total=$(cat bower-packages.json new-packages.json | jq -s "map(length) | add")
-unique=$(cat bower-packages.json new-packages.json | jq -s "map(keys) | add | unique | length"
-)
+bower=$(cat bower-packages.json new-packages.json | jq -s "add | length")
+unique=$(cat bower-packages.json new-packages.json | jq -s "unique | add | length")
 
 if [ "$total" == "$unique" ]; then
   exit 0
