49b36d5df80eb004454a8ec569cb35fb2d95eed0
Thomas Honeyman <admin@thomashoneyman.com>
2022-01-25 11:53:41 -0800

    Update verify-registry-unique.sh

--- verify-registry-unique.sh
+++ verify-registry-unique.sh
@@ -4,9 +4,8 @@
 set -euxo pipefail
 
 # This script checks that there are no duplicate entries in the two json files listing packages
-
-bower=$(cat bower-packages.json new-packages.json | jq -s "add | length")
-unique=$(cat bower-packages.json new-packages.json | jq -s "unique | add | length")
+total=$(cat bower-packages.json new-packages.json | jq -s "map(length) | add")
+unique=$(cat bower-packages.json new-packages.json | jq -s "map(keys) | add | unique | length")
 
 if [ "$total" == "$unique" ]; then
   exit 0
