16f51edd02e8bff2191b5f1944843f8dd6f1df9a
Colin Wahl <colin.t.wahl@gmail.com>
2022-03-30 09:42:13 -0700

    Add fixtures for manifests & generating a tree of manifests

---
+++ ci/test/Fixture/Manifest.purs
@@ -0,0 +1,131 @@
+module Test.Fixture.Manifest where
+
+import Registry.Prelude
+
+import Data.Array as Array
+import Data.Array.NonEmpty as NEA
+import Data.Map as Map
+import Data.Monoid (power)
+import Data.Monoid.Multiplicative (Multiplicative(..))
+import Data.Newtype (unwrap)
+import Data.String as String
+import Foreign.SPDX (License)
+import Foreign.SPDX as SPDX
+import Registry.PackageName (PackageName)
+import Registry.PackageName as PackageName
+import Registry.Schema (Location(..), Manifest(..), Owner(..))
+import Registry.Version (Range, Version)
+import Registry.Version as Version
+
+class Fixture a where
+  fixture :: a
+
+instance Fixture String where
+  fixture = "fixture-string"
+
+instance Fixture PackageName where
+  fixture = unsafeFromRight $ PackageName.parse "fixture-package-name"
+
+instance Fixture Version where
+  fixture = unsafeFromRight $ Version.parseVersion Version.Strict "1.0.0"
+
+instance Fixture License where
+  fixture = unsafeFromRight $ SPDX.parse "MIT"
+
+instance Fixture Owner where
+  fixture = Owner
+    { email: fixture
+    , keytype: fixture
+    , public: fixture
+    }
+
+instance Fixture Location where
+  fixture = GitHub { owner: fixture, repo: fixture, subdir: Nothing }
+
+instance Fixture (Map PackageName Range) where
+  fixture = Map.empty
+
+instance Fixture Manifest where
+  fixture = Manifest
+    { name: fixture
+    , owners: fixture
+    , version: fixture
+    , license: fixture
+    , location: fixture
+    , description: fixture
+    , files: fixture
+    , dependencies: fixture
+    }
+
+instance (Fixture a) => Fixture (Maybe a) where
+  fixture = Just fixture
+
+instance (Fixture a) => Fixture (Array a) where
+  fixture = [ fixture ]
+
+instance (Fixture a) => Fixture (NonEmptyArray a) where
+  fixture = NEA.singleton fixture
+
+updateName :: String -> Manifest -> Manifest
+updateName name (Manifest manifest) =
+  Manifest (manifest { name = unsafeFromRight (PackageName.parse name) })
+
+updateVersion :: String -> Manifest -> Manifest
+updateVersion version (Manifest manifest) =
+  Manifest (manifest { version = unsafeFromRight (Version.parseVersion Version.Strict version)})
+
+updateDependencies :: Array (Tuple String String) -> Manifest -> Manifest
+updateDependencies dependencies (Manifest manifest) =
+  Manifest (manifest { dependencies = dependencies' })
+  where
+  dependencies' = Map.fromFoldable (map go dependencies)
+
+  go (Tuple package version) =
+    Tuple (unsafeFromRight (PackageName.parse package)) (mkRangeIncluding (unsafeFromRight (Version.parseVersion Version.Strict version)))
+
+  mkRangeIncluding :: Version -> Range
+  mkRangeIncluding version =
+    unsafeFromRight (Version.parseRange Version.Strict (String.joinWith "" [">=", Version.printVersion version, " <", Version.printVersion bumpPatch ]))
+    where
+    bumpPatch :: Version
+    bumpPatch =
+      unsafeFromRight (Version.parseVersion Version.Strict (String.joinWith "." (map show [ Version.major version, Version.minor version, Version.patch version + 1 ])))
+
+
+myPackage :: Manifest
+myPackage =
+  fixture
+    # updateName "colin"
+    # updateVersion "2.0.0"
+
+manifestTree :: Int -> Array Manifest
+manifestTree depth =
+  Array.mapWithIndex go init
+  where
+  init :: Array Manifest
+  init = Array.replicate count fixture
+
+  count = unwrap (power (Multiplicative 2) depth) - 1
+
+  left ix = 2 * ix + 1
+
+  right ix = 2 * ix + 2
+
+  name :: Int -> String
+  name ix = String.joinWith "-" [ fixture, show ix ]
+
+  updateName' = updateName <<< name
+
+  go ix manifest = do
+    let
+      deps =
+        [ Tuple (name (left ix)) (Version.printVersion fixture)
+        , Tuple (name (right ix)) (Version.printVersion fixture)
+        ]
+
+      updateDeps =
+        if right ix < count then updateDependencies deps else identity
+
+    manifest
+      # updateName' ix
+      # updateDeps
