f1039b9e6f4a0002a29ae7320116e7752cace3b5
Colin Wahl <colin.t.wahl@gmail.com>
2022-06-29 13:54:07 -0700

    Check for package set inclusion candidates via checking existing package sets & latest versions

--- ci/src/Registry/Scripts/PublishPackageSet.purs
+++ ci/src/Registry/Scripts/PublishPackageSet.purs
@@ -3,6 +3,7 @@ module Registry.Scripts.PublishPackageSet where
 import Registry.Prelude
 
 import Data.Array as Array
+import Data.Array.NonEmpty as NonEmptyArray
 import Data.DateTime as DateTime
 import Data.Int as Int
 import Data.Map as Map
@@ -10,7 +11,6 @@ import Data.PreciseDateTime as PDT
 import Data.Time.Duration (Hours(..))
 import Effect.Aff as Aff
 import Effect.Console as Console
-import Effect.Now (nowDateTime)
 import Effect.Now as Now
 import Effect.Ref as Ref
 import Foreign.Tmp as Tmp
@@ -19,17 +19,10 @@ import Registry.API as API
 import Registry.Json as Json
 import Registry.PackageName (PackageName)
 import Registry.RegistryM (runRegistryM)
-import Registry.Schema (LegacyPackageSet(..))
+import Registry.Schema (LegacyPackageSet(..), LegacyPackageSetEntry(..))
 import Registry.Utils (mkLocalEnv, wget)
 import Registry.Version (Version)
-
-{-
-First, we read the contents of the latest package set release and gather all package versions that have been uploaded to the registry since that release. These package versions are the "batch" of packages that we are considering for automatic inclusion to the next package set.
-
-Second, we filter out any packages where, based on their metadata and manifest files alone, we know they can't be added to the package set. This happens for one of three reasons: they have a dependency that isn't in the package sets, or they had multiple releases since the last package set, in which case we only take the highest version, or c) they already have a higher version number released  in the previous package set. In any of these cases, we remove the package from consideration.
--}
-
--- 
+import Registry.Version as Version
 
 main :: Effect Unit
 main = Aff.launchAff_ do
@@ -43,40 +36,53 @@ main = Aff.launchAff_ do
   API.checkIndexExists
 
   metadata <- liftEffect $ Ref.read packagesMetadataRef
-  liftEffect $ Console.log (show metadata)
 
   now <- liftEffect $ Now.nowDateTime
 
   let
-    newUploads :: Array (Tuple PackageName (Array Version))
+    newUploads :: Array (Tuple PackageName (NonEmptyArray Version))
     newUploads = do
       Tuple packageName packageMetadata <- Map.toUnfoldable metadata
       let
-        versions :: Array Version
-        versions = do
+        versions' :: Array Version
+        versions' = do
           Tuple version { publishedTime } <- Map.toUnfoldable packageMetadata.published
           published <- maybe [] (pure <<< PDT.toDateTimeLossy) (PDT.fromRFC3339String publishedTime)
           let diff = DateTime.diff now published
-          guardA (diff <= Hours (Int.toNumber 100)) -- TODO - 24 or 25 hours lookback
+          guardA (diff <= Hours (Int.toNumber 1000)) -- TODO - 24 or 25 hours lookback
           pure version
 
-      guardA (not (Array.null versions))
-      pure $ Tuple packageName versions
-
-  liftEffect $ Console.log (show newUploads)
+      versions <- Array.fromFoldable (NonEmptyArray.fromArray versions')
+      pure (Tuple packageName versions)
 
   liftEffect $ Console.log "Fetching latest package set..."
 
   runRegistryM (mkLocalEnv packagesMetadataRef) do
     wget "https://raw.githubusercontent.com/purescript/package-sets/master/packages.json" "packages.json"
 
-  (packageSetResult :: Either String LegacyPackageSet) <- Json.readJsonFile "packages.json"
+  packageSetResult :: Either String LegacyPackageSet <- Json.readJsonFile "packages.json"
 
-  packageSet :: LegacyPackageSet <- case packageSetResult of
+  LegacyPackageSet packageSet :: LegacyPackageSet <- case packageSetResult of
     Left err -> unsafeCrashWith err
     Right packageSet -> pure packageSet
 
-  liftEffect $ Console.log (show packageSet)
+  liftEffect $ Console.log "Computing candidates for inclusion in package set..."
 
+  let 
+    uploads :: Array (Tuple PackageName Version)
+    uploads = do
+      Tuple packageName versions' <- newUploads
+      -- We only care about the latest version
+      let versions = NonEmptyArray.reverse (NonEmptyArray.sort versions')
+      let version = NonEmptyArray.head versions
+      case Map.lookup packageName packageSet of
+        Nothing -> pure (Tuple packageName version)
+        Just (LegacyPackageSetEntry { version: RawVersion v' })
+          | Right v <- Version.parseVersion Version.Lenient v'
+          , v < version -> pure (Tuple packageName version)
+        _ -> []
+
+  liftEffect $ Console.log "Found the following uploads eligible for inclusion in package set:"
+  liftEffect $ Console.log (show uploads)
 
   pure unit
