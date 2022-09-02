24db591b992b99730ea23211d746bc4c5fe295d7
Thomas Honeyman <admin@thomashoneyman.com>
2022-09-02 11:47:13 -0400

    Make repo urls configurable (#510)

---
+++ src/Registry/Constants.purs
@@ -0,0 +1,31 @@
+module Registry.Constants
+  ( legacyPackageSetsRepo
+  , metadataPath
+  , packageSetsPath
+  , registryIndexRepo
+  , registryPackagesUrl
+  , registryRepo
+  ) where
+
+import Affjax as Http
+import Node.Path (FilePath)
+
+type Repository = { owner :: String, repo :: String }
+
+registryRepo :: Repository
+registryRepo = { owner: "purescript", repo: "registry-preview" }
+
+packageSetsPath :: FilePath
+packageSetsPath = "package-sets"
+
+metadataPath :: FilePath
+metadataPath = "metadata"
+
+registryIndexRepo :: Repository
+registryIndexRepo = { owner: "purescript", repo: "registry-index" }
+
+legacyPackageSetsRepo :: Repository
+legacyPackageSetsRepo = { owner: "purescript", repo: "package-sets" }
+
+registryPackagesUrl :: Http.URL
+registryPackagesUrl = "https://packages.registry.purescript.org"
