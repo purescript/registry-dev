fcbcdde4c20bf2880b644bf16375599a431c48e4
Thomas Honeyman <hello@thomashoneyman.com>
2022-10-26 10:06:30 -0400

    Update test path

--- flake.nix
+++ flake.nix
@@ -94,15 +94,14 @@
           '';
 
           registry-test = ''
-            cd $(git rev-parse --show-toplevel)/lib
+            cd $(git rev-parse --show-toplevel)
             npm ci
-            cd ..
             spago test
           '';
 
           registry-check-format = ''
             cd $(git rev-parse --show-toplevel)
-            purs-tidy check src test
+            purs-tidy check app lib scripts
           '';
 
           registry-api = ''
