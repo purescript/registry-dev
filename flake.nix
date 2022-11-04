00922a49ffd4cdcdd84c3a532f6d63d945a55a1b
Thomas Honeyman <hello@thomashoneyman.com>
2022-11-03 21:42:02 -0400

    Update to new Spago test stanza

--- flake.nix
+++ flake.nix
@@ -97,7 +97,7 @@
             cd $(git rev-parse --show-toplevel)
             npm ci
             spago test -p registry-app
-            spago test -p registry-lib -m Test.Registry
+            spago test -p registry-lib
           '';
 
           registry-check-format = ''
