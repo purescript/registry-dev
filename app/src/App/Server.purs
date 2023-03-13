ee4b2dbcf77bdc06c0d0ebf9439a7f1704c44ff0
Fabrizio Ferrai <fabrizio.ferrai@gmail.com>
2023-03-13 12:32:59 +0200

    Move to HTTPurple

--- app/src/App/Server.purs
+++ app/src/App/Server.purs
@@ -4,11 +4,12 @@ import Registry.App.Prelude hiding ((/))
 
 import Data.Codec.Argonaut as CA
 import Data.Formatter.DateTime as Formatter.DateTime
+import Data.Lens.Iso.Newtype (_Newtype)
 import Data.String as String
 import Effect.Aff as Aff
 import Effect.Class.Console as Console
-import HTTPure (Method(..))
-import HTTPure as HTTPure
+import HTTPurple (class Generic, Method(..), Request, Response, RouteDuplex', (/))
+import HTTPurple as HTTPurple
 import Node.Path as Path
 import Node.Process as Process
 import Registry.App.API (Source(..))
@@ -37,28 +38,81 @@ import Registry.Foreign.Octokit (GitHubToken, Octokit)
 import Registry.Foreign.Octokit as Octokit
 import Registry.Internal.Format as Internal.Format
 import Registry.Operation as Operation
+import Routing.Duplex as Routing
+import Routing.Duplex.Generic as RoutingG
 import Run (AFF, EFFECT, Run)
 import Run as Run
 import Run.Except (EXCEPT)
 import Run.Except as Except
 
-main :: Effect Unit
-main = do
-  createServerEnv # Aff.runAff_ case _ of
-    Left error -> do
-      Console.log $ "Failed to start server: " <> Aff.message error
-      Process.exit 1
-    Right env -> do
-      _close <- HTTPure.serve 8080 (runServer env router) do
-        Console.log $ String.joinWith "\n"
-          [ " ┌───────────────────────────────────────────┐"
-          , " │ Server now up on port 8080                │"
-          , " │                                           │"
-          , " │ To test, run:                             │"
-          , " │  > curl -v localhost:8080/api/v1/publish  │"
-          , " └───────────────────────────────────────────┘"
-          ]
-      pure unit
+data Route
+  = Publish
+  | Unpublish
+  | Transfer
+  | Jobs JobID
+
+derive instance Generic Route _
+
+newtype JobID = JobID String
+
+instance Newtype JobID String
+
+jobID :: RouteDuplex' JobID
+jobID = _Newtype Routing.segment
+
+routes :: RouteDuplex' Route
+routes = Routing.root $ Routing.prefix "api" $ Routing.prefix "v1" $ RoutingG.sum
+  { "Publish": "publish" / RoutingG.noArgs
+  , "Unpublish": "unpublish" / RoutingG.noArgs
+  , "Transfer": "transfer" / RoutingG.noArgs
+  , "Jobs": "jobs" / jobID
+  }
+
+router :: Request Route -> Run ServerEffects Response
+router { route, method, body } = case route, method of
+  Publish, Post -> do
+    body' <- Run.liftAff $ HTTPurple.toString body
+    case parseJson Operation.publishCodec body' of
+      Left error -> HTTPurple.badRequest $ CA.printJsonDecodeError error
+      Right publish -> do
+        -- TODO: This should really be a launchAff_ acknowledging receipt but
+        -- not actualy processing, once we validate the operation is OK, and we
+        -- can return the job ID for polling.
+        -- So we shall:
+        -- - fork the publishing in a fiber
+        -- - stash the fiber in a ref (so we can keep track of how many things are going)
+        -- - generate a job ID
+        -- - make a log file with that job ID
+        -- - change the Notify effect to write to that log file in a structured format (so we can read it back)
+        API.publish Current publish
+        HTTPurple.ok "Completed publish operation."
+
+  Unpublish, Post -> do
+    body' <- Run.liftAff $ HTTPurple.toString body
+    case parseJson Operation.authenticatedCodec body' of
+      Left error -> HTTPurple.badRequest $ CA.printJsonDecodeError error
+      Right auth -> case auth.payload of
+        Operation.Unpublish _ -> do
+          API.authenticated auth
+          HTTPurple.ok "Completed unpublish operation."
+        _ ->
+          HTTPurple.badRequest "Expected unpublish operation."
+
+  Transfer, Post -> do
+    body' <- Run.liftAff $ HTTPurple.toString body
+    case parseJson Operation.authenticatedCodec body' of
+      Left error -> HTTPurple.badRequest $ CA.printJsonDecodeError error
+      Right auth -> case auth.payload of
+        Operation.Transfer _ -> do
+          API.authenticated auth
+          HTTPurple.ok "Completed transfer operation."
+        _ ->
+          HTTPurple.badRequest "Expected transfer operation."
+
+  Jobs _jobId, _ -> do
+    HTTPurple.ok "TODO"
+
+  _, _ -> HTTPurple.notFound
 
 type ServerEnvVars =
   { token :: GitHubToken
@@ -108,7 +162,7 @@ createServerEnv = do
 
 type ServerEffects = (PACCHETTIBOTTI_ENV + REGISTRY + GITHUB + GIT + STORAGE + PURSUIT + LEGACY_CACHE + NOTIFY + LOG + EXCEPT String + AFF + EFFECT ())
 
-runServer :: ServerEnv -> (HTTPure.Request -> Run ServerEffects HTTPure.Response) -> HTTPure.Request -> Aff HTTPure.Response
+runServer :: ServerEnv -> (Request Route -> Run ServerEffects Response) -> Request Route -> Aff Response
 runServer env router' request = do
   now <- nowUTC
   let logFile = String.take 19 (Formatter.DateTime.format Internal.Format.iso8601DateTime now) <> "-" <> String.joinWith "__" request.path <> ".log"
@@ -137,42 +191,33 @@ runServer env router' request = do
       # Run.runBaseAff'
 
   case result of
-    Left error -> HTTPure.badRequest (Aff.message error)
+    Left error -> HTTPurple.badRequest (Aff.message error)
     Right response -> pure response
 
-router :: HTTPure.Request -> Run ServerEffects HTTPure.Response
-router match = case match.path of
-  [ "api", "v1", "publish" ] | match.method == Post -> do
-    body <- Run.liftAff $ HTTPure.toString match.body
-    case parseJson Operation.publishCodec body of
-      Left error -> HTTPure.badRequest $ CA.printJsonDecodeError error
-      Right publish -> do
-        -- TODO: This should really be a launchAff_ acknowledging receipt but
-        -- not actualy processing, once we validate the operation is OK, and we
-        -- can return the job ID for polling.
-        API.publish Current publish
-        HTTPure.ok "Completed publish operation."
-
-  [ "api", "v1", "unpublish" ] | match.method == Post -> do
-    body <- Run.liftAff $ HTTPure.toString match.body
-    case parseJson Operation.authenticatedCodec body of
-      Left error -> HTTPure.badRequest $ CA.printJsonDecodeError error
-      Right auth -> case auth.payload of
-        Operation.Unpublish _ -> do
-          API.authenticated auth
-          HTTPure.ok "Completed unpublish operation."
-        _ ->
-          HTTPure.badRequest "Expected unpublish operation."
-
-  [ "api", "v1", "transfer" ] | match.method == Post -> do
-    body <- Run.liftAff $ HTTPure.toString match.body
-    case parseJson Operation.authenticatedCodec body of
-      Left error -> HTTPure.badRequest $ CA.printJsonDecodeError error
-      Right auth -> case auth.payload of
-        Operation.Transfer _ -> do
-          API.authenticated auth
-          HTTPure.ok "Completed transfer operation."
-        _ ->
-          HTTPure.badRequest "Expected transfer operation."
-
-  _ -> HTTPure.notFound
+main :: Effect Unit
+main = do
+  createServerEnv # Aff.runAff_ case _ of
+    Left error -> do
+      Console.log $ "Failed to start server: " <> Aff.message error
+      Process.exit 1
+    Right env -> do
+      _close <- HTTPurple.serve
+        { hostname: "0.0.0.0"
+        , port: 8080
+        , onStarted
+        }
+        { route: routes
+        , router: runServer env router
+        }
+      pure unit
+  where
+  onStarted :: Effect Unit
+  onStarted = do
+    Console.log $ String.joinWith "\n"
+      [ " ┌───────────────────────────────────────────┐"
+      , " │ Server now up on port 8080                │"
+      , " │                                           │"
+      , " │ To test, run:                             │"
+      , " │  > curl -v localhost:8080/api/v1/publish  │"
+      , " └───────────────────────────────────────────┘"
+      ]
