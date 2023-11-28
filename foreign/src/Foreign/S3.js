0b9e00625111c20506a536ea3fbb51f9687150bf
Thomas Honeyman <hello@thomashoneyman.com>
2023-11-28 16:02:08 -0500

    Update to new S3 bindings

--- foreign/src/Foreign/S3.js
+++ foreign/src/Foreign/S3.js
@@ -1,48 +1,32 @@
-import aws from "aws-sdk";
+import {
+  DeleteObjectCommand,
+  ListObjectsCommand,
+  PutObjectCommand,
+  S3,
+} from "@aws-sdk/client-s3";
 
-export const connectImpl = ({ key, secret }, endpoint) => {
-  const spacesEndpoint = new aws.Endpoint(endpoint);
-  return new aws.S3({
-    endpoint: spacesEndpoint,
-    accessKeyId: key,
-    secretAccessKey: secret,
+export const connectImpl = ({ key, secret }, endpoint) =>
+  new S3({
+    forcePathStyle: false,
+    endpoint,
+    region: "us-east-1",
+    credentials: {
+      accessKeyId: key,
+      secretAccessKey: secret,
+    },
   });
+
+export const listObjectsImpl = async (s3, params) => {
+  const data = await s3.send(new ListObjectsCommand(params));
+  return data["Contents"];
 };
 
-// TODO: we should switch to v3 of the SDK:
-// https://docs.aws.amazon.com/AWSJavaScriptSDK/v3/latest/clients/client-s3/classes/listobjectsv2command.html
-export const listObjectsImpl = (s3, params) => {
-  return new Promise(function (resolve, reject) {
-    s3.listObjectsV2(params, function (err, data) {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(data["Contents"]);
-      }
-    });
-  });
+export const putObjectImpl = async (s3, params) => {
+  const data = await s3.send(new PutObjectCommand(params));
+  return data;
 };
 
-export const putObjectImpl = (s3, params) => {
-  return new Promise(function (resolve, reject) {
-    s3.putObject(params, function (err, data) {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(data);
-      }
-    });
-  });
-};
-
-export const deleteObjectImpl = (s3, params) => {
-  return new Promise(function (resolve, reject) {
-    s3.deleteObject(params, function (err, data) {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(data);
-      }
-    });
-  });
+export const deleteObjectImpl = async (s3, params) => {
+  const data = await s3.send(new DeleteObjectCommand(params));
+  return data;
 };
