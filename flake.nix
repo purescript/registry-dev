103d26ac87f49d24284ef7ab2fc7b0ae19ed8acd
Fabrizio Ferrai <fabrizio.ferrai@gmail.com>
2024-08-29 17:45:27 +0300

    Back to spago 0.93.19

--- flake.nix
+++ flake.nix
@@ -270,7 +270,7 @@
 
           pushd $WORKDIR
           export HEALTHCHECKS_URL=${defaultEnv.HEALTHCHECKS_URL}
-          ${pkgs.spago-bin.spago-0_93_39}/bin/spago test
+          ${pkgs.spago-bin.spago-0_93_19}/bin/spago test
 
           popd
         '';
@@ -853,7 +853,7 @@
 
               # Development tooling
               purs
-              spago-bin.spago-0_93_39
+              spago-bin.spago-0_93_19 # until new lockfile format supported by overlay
               purs-tidy-unstable
               purs-backend-es-unstable
             ];
