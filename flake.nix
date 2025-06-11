f28ba70d3a8e6d87946b7e6d51433f8ee0a64e0b
Fabrizio Ferrai <fabrizio.ferrai@gmail.com>
2025-06-11 17:01:15 +0000

    Update to nix 25.5

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "The PureScript Registry";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/release-24.05";
+    nixpkgs.url = "github:nixos/nixpkgs/release-25.05";
     flake-utils.url = "github:numtide/flake-utils";
 
     flake-compat.url = "github:edolstra/flake-compat";
@@ -10,6 +10,10 @@
 
     purescript-overlay.url = "github:thomashoneyman/purescript-overlay";
     purescript-overlay.inputs.nixpkgs.follows = "nixpkgs";
+
+    mkSpagoDerivation.url = "github:jeslie0/mkSpagoDerivation";
+    mkSpagoDerivation.inputs.nixpkgs.follows = "nixpkgs";
+    mkSpagoDerivation.inputs.ps-overlay.follows = "purescript-overlay";
 
     slimlock.url = "github:thomashoneyman/slimlock";
     slimlock.inputs.nixpkgs.follows = "nixpkgs";
@@ -21,6 +25,7 @@
       nixpkgs,
       flake-utils,
       purescript-overlay,
+      mkSpagoDerivation,
       slimlock,
       ...
     }:
@@ -44,6 +49,7 @@
           ./foreign
           ./lib
           ./scripts
+          ./test-utils
           ./spago.lock
           ./spago.yaml
         ]
@@ -76,7 +82,7 @@
         # We don't want to force everyone to update their configs if they aren't
         # normally on flakes.
         nixFlakes = prev.writeShellScriptBin "nixFlakes" ''
-          exec ${prev.nixFlakes}/bin/nix --experimental-features "nix-command flakes" "$@"
+          exec ${prev.nixVersions.stable}/bin/nix --experimental-features "nix-command flakes" "$@"
         '';
 
         # Detects arguments to 'git' containing a URL and replaces them with a
@@ -144,12 +150,12 @@
         # Packages associated with the registry, ie. in this repository.
         registry =
           let
-            spago-lock = prev.purix.buildSpagoLock {
-              src = fileset.toSource {
-                root = ./.;
-                fileset = pureScriptFileset;
-              };
-              corefn = true;
+            spago-lock = prev.mkSpagoDerivation {
+              name = "registry";
+              src = ./.;
+              nativeBuildInputs = [ prev.pkgs.spago-bin.spago-0_93_44 prev.pkgs.purescript ];
+              buildPhase = "spago build";
+              installPhase = "mkdir $out; cp -r * $out";
             };
 
             package-lock =
@@ -245,6 +251,7 @@
           inherit system;
           overlays = [
             purescript-overlay.overlays.default
+            mkSpagoDerivation.overlays.default
             slimlock.overlays.default
             registryOverlay
           ];
@@ -317,6 +324,7 @@
                 {
                   nixpkgs.overlays = [
                     purescript-overlay.overlays.default
+                    mkSpagoDerivation.overlays.default
                     slimlock.overlays.default
                     registryOverlay
                   ];
@@ -870,6 +878,7 @@
             system = "x86_64-linux";
             overlays = [
               purescript-overlay.overlays.default
+              mkSpagoDerivation.overlays.default
               slimlock.overlays.default
               registryOverlay
             ];
